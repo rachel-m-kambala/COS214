62e5e42d1833e4f55a1c93321437d85bc97b4797
rachel-m-kambala <rachel.m.kambala@gmail.com>
2025-08-09 16:17:02 +0200

    h.files

---
+++ Practical1/RectangleFactory.h
@@ -0,0 +1,13 @@
+//Mukaji Mweni Rachel Kambala u23559129
+//Jerusha Thaver u23686376
+
+#ifndef RECTANGLEFACTORY.h
+#define RECTANGLEFACTORY.h
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <list>
+
+#endif
