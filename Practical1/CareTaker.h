550977655d82f4ebe7a6370a4d2be761a9325195
JerushaThaver <thaverjerusha19@gmail.com>
2025-08-09 22:06:42 +0200

    Update CareTaker.h

--- Practical1/CareTaker.h
+++ Practical1/CareTaker.h
@@ -4,19 +4,6 @@
 #include "Memento.h"
 
 class CareTaker {
-<<<<<<< HEAD
-   public:
-        CareTaker(int c = 100);
-        ~CareTaker();
-        
-        void addMemento(Memento* m);
-        Memento* getLastMemento();
-   
-    private:
-        Memento** history;
-        int count;
-        int capacity;
-=======
 private:
     Memento** history;
     int size;
@@ -26,7 +13,6 @@ public:
     ~CareTaker();
     void addMemento(Memento* m);
     Memento* getMemento(int index);
->>>>>>> 473863fd1baead4c5373a4fc649f04919ad01bb9
 };
 
 #endif
