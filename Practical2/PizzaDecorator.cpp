0a2bcbb69a3dfd10de3d5648b6a3136b2d6acfe9
JerushaThaver <thaverjerusha19@gmail.com>
2025-08-29 16:01:50 +0200

    Update PizzaDecorator.cpp

--- Practical2/PizzaDecorator.cpp
+++ Practical2/PizzaDecorator.cpp
@@ -3,21 +3,18 @@
 
 #include "PizzaDecorator.h"
 
-// Constructor: store pizza pointer and optional cost
 PizzaDecorator::PizzaDecorator(Pizza* p, double cost)
     : pizza(p), extraPrice(cost) {}
 
-// Destructor: delete wrapped pizza
 PizzaDecorator::~PizzaDecorator() {
     delete pizza;
 }
 
-// Default implementation: just returns the wrapped pizza's price + extra
 double PizzaDecorator::getPrice() {
     return pizza->getPrice() + extraPrice;
 }
 
-// Default implementation: just returns the wrapped pizza's name
+
 std::string PizzaDecorator::getName() {
     return pizza->getName();
 }
