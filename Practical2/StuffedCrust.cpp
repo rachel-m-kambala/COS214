873d6c7c4d98909290e85d46041123cb446b1ab1
JerushaThaver <thaverjerusha19@gmail.com>
2025-08-29 15:47:31 +0200

    Update StuffedCrust.cpp

--- Practical2/StuffedCrust.cpp
+++ Practical2/StuffedCrust.cpp
@@ -2,3 +2,22 @@
 //JERUSHA THAVER u23686376
 
 #include "StuffedCrust.h"
+#include <iostream>
+using namespace std;
+
+StuffedCrust::StuffedCrust(Pizza* p, double cost)
+    : PizzaDecorator(p), extraPrice(cost) {}
+
+StuffedCrust::~StuffedCrust() {}
+
+double StuffedCrust::getPrice() {
+    return pizza->getPrice() + extraPrice;
+}
+
+string StuffedCrust::getName() {
+    return pizza->getName() + " + Stuffed Crust";
+}
+
+void StuffedCrust::printPizza() {
+    cout << getName() << " -> Price: " << getPrice() << endl;
+}
