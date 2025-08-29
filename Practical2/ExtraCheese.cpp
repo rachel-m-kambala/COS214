96d844495361a485db565ab96bd2438afd6454bd
JerushaThaver <thaverjerusha19@gmail.com>
2025-08-29 15:45:25 +0200

    ExtraCheese

--- Practical2/ExtraCheese.cpp
+++ Practical2/ExtraCheese.cpp
@@ -2,3 +2,22 @@
 //JERUSHA THAVER u23686376
 
 #include "ExtraCheese.h"
+#include <iostream>
+using namespace std;
+
+ExtraCheese::ExtraCheese(Pizza* p, double cost)
+    : PizzaDecorator(p), extraPrice(cost) {}
+
+ExtraCheese::~ExtraCheese() {}
+
+double ExtraCheese::getPrice() {
+    return pizza->getPrice() + extraPrice;
+}
+
+string ExtraCheese::getName() {
+    return pizza->getName() + " + Extra Cheese";
+}
+
+void ExtraCheese::printPizza() {
+    cout << getName() << " -> Price: " << getPrice() << endl;
+}
