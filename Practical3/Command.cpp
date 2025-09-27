9921f491495d52a49415835fa0e5161ad030ff6b
JerushaThaver <thaverjerusha19@gmail.com>
2025-09-27 17:47:37 +0200

    Command.h

---
+++ Practical3/Command.cpp
@@ -0,0 +1,3 @@
+#include "Command.h"
+//Jerusha Thaver u23686376
+//Mukaji Mweni Rachel Kambala u23559129
--- Practical3/Command.h
+++ Practical3/Command.h
@@ -3,19 +3,23 @@
 #ifndef COMMAND_H
 #define COMMAND_H
 
-#include <iostream>
-#include <vector>
-#include <map>
-#include <list>
+#include <string>
 
-class SendMessageCommand{
-    public:
-        void execute();
-    
-    protected:
-        ChatRoom room;
-        std::string message;
-        User fromUser;
-}
+class ChatRoom;
+class User;
+
+class Command {
+protected:
+    ChatRoom* chatRoom;
+    User* fromUser;
+    std::string message;
+
+public:
+    Command(ChatRoom* room, User* user, const std::string& msg)
+        : chatRoom(room), fromUser(user), message(msg) {}
+
+    virtual ~Command() {}
+    virtual void execute() = 0;
+};
 
 #endif
